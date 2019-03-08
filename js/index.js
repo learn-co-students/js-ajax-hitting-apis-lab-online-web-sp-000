bba883cb5be1080c52a91b6e5b7841909d043ce8
github username <github email address>
2019-03-07 23:52:15 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,17 +1,23 @@
 // your code here
 function displayCommits(){
-
+  let commits = JSON.parse(this.responseText);
+  const commitsList = `<ul> ${commits.map ( commit =>
+  '<li>' + commit.commit.author.name + ' - ' + commit.author.login + ' - ' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById('details').innerHTML = commitsList;
 }
 
 function displayBranches() {
-
+  const branches = JSON.parse(this.responseText);
+  const branchesList = `<ul>${branches.map(branch =>
+    '<li>' + branch.name + '</li>').join('')}</ul>`;
+  document.getElementById('details').innerHTML = branchesList;
 }
 
 function displayRepositories() {
   let repos = JSON.parse(this.responseText);
   console.log(repos);
   const repoList = `<ul> ${repos.map( repo => '<li>' + '<a href="' + repo.html_url + '">' + repo.name +
-  '</a> <a href="#" data-username="'+ repo.owner.login + '" data-repo="' + repo.name +'" onclick="getCommits(this)">Get Commits</a></li>' ).join('')}</ul>`
+  '</a> <a href="#" data-username="'+ repo.owner.login + '" data-repository="' + repo.name +'" onclick="getCommits(this)">Get Commits</a></li>' ).join('')}</ul>`
   document.getElementById('repositories').innerHTML = repoList;
 }
 
@@ -25,15 +31,20 @@ function getRepositories() {
 
 function getCommits(repo) {
   const request = new XMLHttpRequest;
-  let url = 'https://api.github.com/repos/' + repo.dataset.username + '/' + repo.dataset.repo + '/commits'
+  let url = 'https://api.github.com/repos/' + repo.dataset.username + '/' + repo.dataset.repository + '/commits'
   console.log(repo.dataset.username)
-  console.log(repo.dataset.repo)
+  console.log(repo.dataset.repository)
   console.log(url);
   request.addEventListener('load', displayCommits);
   request.open('GET', url);
   request.send();
 }
 
-function getBranches() {
+function getBranches(repo) {
+  const request = new XMLHttpRequest;
+  let url = 'https://api.github.com/repos/' + repo.dataset.username + '/' + repo.dataset.repository + '/branches'
+  request.addEventListener('load', displayBranches);
+  request.open('GET', url);
+  request.send();
 
 }
